547a218f60e2da708dade11e4dc27d29bf143053
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-08-28 00:05:14 +0600

    major updated

---
+++ main.js
@@ -0,0 +1,33 @@
+// common function for show/hide nested dropdown
+function takeElementsById(elementId){
+  let milestone = document.getElementById(elementId);
+  if (milestone.style.display === "block") {
+    milestone.style.display = "none";
+  } else {
+    milestone.style.display = "block";
+  }
+}
+
+function milestoneOne() {
+  takeElementsById("nested-dropdown1");
+}
+
+function milestoneTwo() {
+  takeElementsById("nested-dropdown2");
+}
+
+function milestoneThree() {
+  takeElementsById("nested-dropdown3");
+}
+
+function milestoneFour() {
+  takeElementsById("nested-dropdown4");
+}
+
+function milestoneFive() {
+  takeElementsById("nested-dropdown5");
+}
+
+function milestoneSix() {
+  takeElementsById("nested-dropdown6");
+}
